6628398d54a7cd28dbc367f82fe32d8e5ff86e4b
Sebastian Torney <STorney@web.de>
2024-04-23 22:11:52 +0200

    offset

--- js/models/drawable-object.class.js
+++ js/models/drawable-object.class.js
@@ -21,16 +21,6 @@ class DrawableObj {
         }
     }
 
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken || this instanceof ChickenSmall
-            || this instanceof ThrowableObj || this instanceof Bottle || this instanceof Coin) {
-            ctx.beginPath();
-            ctx.lineWidth = '2';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-    }
 
     drawFrame(ctx) {
         if (this instanceof Character || this instanceof Chicken || this instanceof ChickenSmall
